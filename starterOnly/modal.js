ee0928a4478c29a7587e060123b23b9eb788565e
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-08-16 10:13:08 +0200

    #1 fermer la modal OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,24 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const xButton = document.querySelector(".close"); //bouton croix
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+xButton.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
 
 
