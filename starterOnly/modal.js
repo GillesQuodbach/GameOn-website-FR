605eac58c2bf9e6c069b5f1944befef58bb5d728
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-08-20 18:11:34 +0200

    fonction valider ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,47 +11,120 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+//VARIABLES
 const xButton = document.querySelector(".close"); // Bouton croix
+const subButton = document.querySelector(".btn-submit"); //Bouton submit
 
 
-let firstName = document.getElementById("firstName"); // Prénom
-let lastName = document.getElementById("lastName"); // Nom
-let email = document.getElementById("email"); //Email
-let birthdate = document.getElementById("birthdate"); //Date de naissance
-let myForm = document.getElementById("reserve");
-let regExp = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+let firstName = document.querySelector("#firstName"); // Prénom
+let lastName = document.querySelector("#lastName"); // Nom
+let email = document.querySelector("#email"); //Email
+let birthdate = document.querySelector("#birthdate"); //Date de naissance
+let nbrTournaments = document.querySelector("#quantity"); //Nbr tournois
+let myForm = document.querySelector("#reserve"); //Formulaire complet
+let emailError = document.querySelector("#mail-error"); //Erreur email
+let firstNameError = document.querySelector('#firstname-error'); //Erreur prénom
+let lastNameError = document.querySelector('#lastname-error'); //Erreur nom
+let tournamentError = document.querySelector('#tournament-error');
+
+//REGEXP
+let emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let tournamentRegExp = /^[0-9]+$/;
+//Test RegExp mail
+let testEmail = emailRegExp.test(email.value);
+//MODAL
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // close modal event
 xButton.addEventListener("click", closeModal);
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
+//VALIDATION FORMULAIRE
+//Ecoute de la modification du PRENOM
+// firstName.addEventListener('change', function() {
+//   firstNameValidation(this);
+// });
+// const firstNameValidation = (function(inputFirstName){
+//   if ((firstName.value === "") || (firstName.value.length <= 2)){
+//     firstNameError.innerHTML = "Prénom invalide";
+//   } else {
+//     firstNameError.innerHTML = "";
+//   }
+// })
+//
+//Ecoute de la modification du NOM
+// lastName.addEventListener('change', function(){
+//   lastNameValidation(this);
+// });
+// const lastNameValidation = (function(inputLastName){
+//   if ((lastName.value === "") || (lastName.value.length <= 2)){
+//     lastNameError.innerHTML = "Nom invalide";
+//   } else {
+//     lastNameError.innerHTML = "";
+//   }
+// });
+//
+//Ecoute de la modification du MAIL
+// email.addEventListener('change', function(){
+//   emailValidation(this);
+// });
+// Test REGEXP
+// const emailValidation = function(inputEmail) {
+//   let testEmail = emailRegExp.test(inputEmail.value);
+//   if (testEmail === true){
+//     emailError.innerHTML = "";
+//   } else {
+//     emailError.innerHTML = "Adresse invalide";
+//   }
+// };
+
+//Ecoute de la modification du nombre de TOURNOIS
+// nbrTournaments.addEventListener('change', function(){
+//   nrbTournamentsValidation(this);
+// })
+// const nrbTournamentsValidation = function(inputTournament) {
+//   if (inputTournament.value === "") {
+//     tournamentError.innerHTML = "Merci de rentrer un chiffre";
+//   } else {
+//     tournamentError.innerHTML = "";
+//   }
+// }
 
 
-
-
-function validate(){
-  if ((firstName.value === '') || (firstName.value.length <=2)) {
-    alert(`Votre prénom n'est pas conforme !`)
+function validate() {
+  if (firstName.value == "") {
+    firstNameError.innerHTML = "Entrez votre prenom";
     return false;
   }
-  if ((lastName.value === '') || (lastName.value.length <=2)) {
-    alert(`Votre nom n'est pas conforme !`)
+  if (lastName.value == "") {
+    lastNameError.innerHTML = "Entrez votre nom";
     return false;
   }
 
+if ((testEmail === false) || (email.value === "")){
+  emailError.innerHTML = "Entrez votre mail";
+  return false;
+  }
+  if (birthdate.value === "jj/mm/aaaa") {
+    emailError.innerHTML = "Entrez une date valide";
+    return false;
+  }
+  if (nbrTournaments.value === "") {
+    tournamentError.innerHTML = "Entrez un nombre";
+    return false;
+  }
+
+  return true;
 }
-
 
 
 
