b446d9e295552087872058adc1aaf4f01f9159d3
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-08-22 17:17:36 +0200

    validation formulaire ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,9 +14,7 @@ const formData = document.querySelectorAll(".formData");
 
 //VARIABLES
 const xButton = document.querySelector(".close"); // Bouton croix
-const subButton = document.querySelector(".btn-submit"); //Bouton submit
-
-
+// const subButton = document.querySelector(".btn-submit"); //Bouton submit
 
 let firstName = document.querySelector("#firstName"); // Prénom
 let lastName = document.querySelector("#lastName"); // Nom
@@ -24,16 +22,20 @@ let email = document.querySelector("#email"); //Email
 let birthdate = document.querySelector("#birthdate"); //Date de naissance
 let nbrTournaments = document.querySelector("#quantity"); //Nbr tournois
 let myForm = document.querySelector("#reserve"); //Formulaire complet
-let emailError = document.querySelector("#mail-error"); //Erreur email
-let firstNameError = document.querySelector('#firstname-error'); //Erreur prénom
-let lastNameError = document.querySelector('#lastname-error'); //Erreur nom
-let tournamentError = document.querySelector('#tournament-error');
-
+let emailError = document.querySelector(".mail-error"); //Erreur email
+let firstNameError = document.querySelector('.firstname-error'); //Erreur prénom
+let lastNameError = document.querySelector('.lastname-error'); //Erreur nom
+let tournamentError = document.querySelector('.tournament-error'); //Erreur nbr tournois
+let radioError = document.querySelector(".radio-button-error");//Erreur checkbox
+let radioButton = document.forms[0].location;//Radio button
+let birthdateError = document.querySelector(".birthdate-error");
+let checkboxError = document.querySelector(".checkbox-error");
+let checkbox = document.querySelector("#checkbox1");
+let errorMsg = document.querySelectorAll(".errormsg");
 //REGEXP
 let emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-let tournamentRegExp = /^[0-9]+$/;
-//Test RegExp mail
-let testEmail = emailRegExp.test(email.value);
+let dateRegExp =/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
+
 //MODAL
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -49,80 +51,53 @@ function closeModal() {
 }
 
 //VALIDATION FORMULAIRE
-//Ecoute de la modification du PRENOM
-// firstName.addEventListener('change', function() {
-//   firstNameValidation(this);
-// });
-// const firstNameValidation = (function(inputFirstName){
-//   if ((firstName.value === "") || (firstName.value.length <= 2)){
-//     firstNameError.innerHTML = "Prénom invalide";
-//   } else {
-//     firstNameError.innerHTML = "";
-//   }
-// })
-//
-//Ecoute de la modification du NOM
-// lastName.addEventListener('change', function(){
-//   lastNameValidation(this);
-// });
-// const lastNameValidation = (function(inputLastName){
-//   if ((lastName.value === "") || (lastName.value.length <= 2)){
-//     lastNameError.innerHTML = "Nom invalide";
-//   } else {
-//     lastNameError.innerHTML = "";
-//   }
-// });
-//
-//Ecoute de la modification du MAIL
-// email.addEventListener('change', function(){
-//   emailValidation(this);
-// });
-// Test REGEXP
-// const emailValidation = function(inputEmail) {
-//   let testEmail = emailRegExp.test(inputEmail.value);
-//   if (testEmail === true){
-//     emailError.innerHTML = "";
-//   } else {
-//     emailError.innerHTML = "Adresse invalide";
-//   }
-// };
-
-//Ecoute de la modification du nombre de TOURNOIS
-// nbrTournaments.addEventListener('change', function(){
-//   nrbTournamentsValidation(this);
-// })
-// const nrbTournamentsValidation = function(inputTournament) {
-//   if (inputTournament.value === "") {
-//     tournamentError.innerHTML = "Merci de rentrer un chiffre";
-//   } else {
-//     tournamentError.innerHTML = "";
-//   }
-// }
-
-
 function validate() {
+  // Validation Prenom
   if (firstName.value == "") {
     firstNameError.innerHTML = "Entrez votre prenom";
     return false;
   }
+  // Validation Nom
   if (lastName.value == "") {
     lastNameError.innerHTML = "Entrez votre nom";
     return false;
   }
-
-if ((testEmail === false) || (email.value === "")){
-  emailError.innerHTML = "Entrez votre mail";
-  return false;
-  }
-  if (birthdate.value === "jj/mm/aaaa") {
-    emailError.innerHTML = "Entrez une date valide";
+  //Validation Mail
+  let testEmail = emailRegExp.test(email.value);
+  if (testEmail === false) {
+    emailError.innerHTML = "Adresse invalide";
     return false;
   }
+  //Validation Date de naissance
+  let testDate = dateRegExp.test(birthdate.value);
+  if (testDate === false) {
+    birthdateError.innerHTML = "Entrez une date valide";
+    return false;
+  }
+  //Validation nombre de tournois
   if (nbrTournaments.value === "") {
     tournamentError.innerHTML = "Entrez un nombre";
     return false;
   }
-
+  //Validation Radio Button
+  let valid = false;
+for(let i = 0; i < radioButton.length; i++) {
+  if (radioButton[i].checked){
+    valid = true;
+    break;
+  }
+}
+if(valid) {
+  radioError.innerHTML = ""
+} else {
+  radioError.innerHTML = "Cocher une case";
+  return false;
+}
+//Validation Checkbox
+  if (checkbox.checked === false) {
+    checkboxError.innerHTML = "Valider les conditions"
+    return false;
+  }
   return true;
 }
 
