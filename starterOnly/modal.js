2cf6f23a8daa5f63bb106c0161957136c968f99e
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-08-25 18:57:40 +0200

    Modal s'ouvre et se ferme instant

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,7 +21,11 @@ let lastName = document.querySelector("#lastName"); // Nom
 let email = document.querySelector("#email"); //Email
 let birthdate = document.querySelector("#birthdate"); //Date de naissance
 let nbrTournaments = document.querySelector("#quantity"); //Nbr tournois
-let myForm = document.querySelector("#reserve"); //Formulaire complet
+let myForm = document.getElementById("reserve"); //Formulaire complet SUBMIT
+
+
+
+//Formulaire complet
 let emailError = document.querySelector(".mail-error"); //Erreur email
 let firstNameError = document.querySelector('.firstname-error'); //Erreur prénom
 let lastNameError = document.querySelector('.lastname-error'); //Erreur nom
@@ -31,20 +35,38 @@ let radioButton = document.forms[0].location;//Radio button
 let birthdateError = document.querySelector(".birthdate-error");
 let checkboxError = document.querySelector(".checkbox-error");
 let checkbox = document.querySelector("#checkbox1");
-let errorMsg = document.querySelectorAll(".errormsg");
+
 //REGEXP
 let emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 let dateRegExp =/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
+
+//ThanksMODAL CONST
+const thanksModalContainer = document.querySelector(".thanks-modal-container"); //Modal container
+const closeThanksModalBtn = document.querySelector(".thanks-modal-close-btn") //Modal close button
+
+// close thanks-modal FUNCTION
+function closeThanksModal() {
+  thanksModalContainer.style.display = "none";
+}
+// close thanks-modal EVENT
+closeThanksModalBtn.addEventListener("click", closeThanksModal);
+
+
+//launch thanks-modal FUNCTION
+function launchThanksModal(){
+  thanksModalContainer.style.display = "block";
+}
+
 //MODAL
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// close modal event
-xButton.addEventListener("click", closeModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close modal event
+xButton.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
@@ -52,32 +74,33 @@ function closeModal() {
 
 //VALIDATION FORMULAIRE
 function validate() {
+  launchThanksModal();
   // Validation Prenom
-  if (firstName.value == "") {
+  if (firstName.value === "") {
     firstNameError.innerHTML = "Votre prénom doit contenir 2 caractères ou plus.";
-    return false;
+    // return false;
   }
   // Validation Nom
-  if (lastName.value == "") {
+  if (lastName.value === "") {
     lastNameError.innerHTML = "Votre nom doit contenir 2 caractères ou plus!";
-    return false;
+    // return false;
   }
   //Validation Mail
   let testEmail = emailRegExp.test(email.value);
-  if (testEmail === false) {
+  if ((testEmail === false) || (email.value === "")) {
     emailError.innerHTML = "Veuillez saisir une adresse email valide.";
-    return false;
+    // return false;
   }
   //Validation Date de naissance
   let testDate = dateRegExp.test(birthdate.value);
   if (testDate === false) {
     birthdateError.innerHTML = "Veuillez entrer votre date de naissance.";
-    return false;
+    // return false;
   }
   //Validation nombre de tournois
   if (nbrTournaments.value === "") {
     tournamentError.innerHTML = "Veuillez entrer un nombre";
-    return false;
+    // return false;
   }
   //Validation Radio Button
   //Boucle vérifier
@@ -92,17 +115,19 @@ if(valid) {
   radioError.innerHTML = ""
 } else {
   radioError.innerHTML = "Veuillez choisir une option";
-  return false;
+  // return false;
 }
 //Validation Checkbox
   if (checkbox.checked === false) {
     checkboxError.innerHTML = "Veuillez lire et valider les conditions."
-    return false;
+    // return false;
   }
-  alert('Merci ! Votre réservation a bien été envoyée.')
-  return true;
+if ((!valid||!testDate||!testEmail||!checkbox.checked)||(lastName.value === "")||(firstName.value === "")||(nbrTournaments.value === "")) {
+  return false;
 }
 
+  return true;
+}
 
 
 
