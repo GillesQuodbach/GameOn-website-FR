8d0edc6c6c00bc8233cc0e5bea16fdd8a2175856
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-08-27 20:48:59 +0200

    Reste soucis Modal + css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,14 +14,12 @@ const formData = document.querySelectorAll(".formData");
 
 //VARIABLES
 const xButton = document.querySelector(".close"); // Bouton croix
-// const subButton = document.querySelector(".btn-submit"); //Bouton submit
-
+const myForm = document.querySelector("#reserve");
 let firstName = document.querySelector("#firstName"); // Prénom
 let lastName = document.querySelector("#lastName"); // Nom
 let email = document.querySelector("#email"); //Email
 let birthdate = document.querySelector("#birthdate"); //Date de naissance
 let nbrTournaments = document.querySelector("#quantity"); //Nbr tournois
-let myForm = document.getElementById("reserve"); //Formulaire complet SUBMIT
 
 
 
@@ -39,25 +37,6 @@ let checkbox = document.querySelector("#checkbox1");
 //REGEXP
 let emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 let dateRegExp =/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
-
-
-//ThanksMODAL CONST
-const thanksModalContainer = document.querySelector(".thanks-modal-container"); //Modal container
-const closeThanksModalBtn = document.querySelector(".thanks-modal-close-btn") //Modal close button
-
-// close thanks-modal FUNCTION
-function closeThanksModal() {
-  thanksModalContainer.style.display = "none";
-}
-// close thanks-modal EVENT
-closeThanksModalBtn.addEventListener("click", closeThanksModal);
-
-
-//launch thanks-modal FUNCTION
-function launchThanksModal(){
-  thanksModalContainer.style.display = "block";
-}
-
 //MODAL
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -71,10 +50,59 @@ xButton.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
+//===================================================================================
+// FORMULAIRE ENTIER
 
+
+
+
+//ThanksMODAL container
+const thanksModalContainer = document.querySelector(".thanks-modal-container");
+//BOUTON OK
+const closeThanksModalBtn = document.querySelector(".thanks-modal-close-btn");
+//CLOSE ThanksMODAL CONST
+//Modal close button
+closeThanksModalBtn.addEventListener("click", closeThanksModal);
+// close thanks-modal FUNCTION
+function closeThanksModal() {
+  thanksModalContainer.style.display = "none";
+}
+// APPARITION MODAL MERCI
+// function launchThanksModale (){
+//   if (validate===true) {
+//   thanksModalContainer.style.display = "block";
+//   }
+// }
+
+//======================================================================================
 //VALIDATION FORMULAIRE
+
+// RAJOUTER ICI LE TEXT DE L4AUTRE ECRAN
+let putain = document.getElementById("detail");
+putain.addEventListener("click",launchThanksModale);
+
+function launchThanksModale (){
+    thanksModalContainer.style.display = "block";
+}
+
+// ESSAI 1========================================
+//   myForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     alert('Please enter your');
+//   });
+// ESSAI 2========================================
+// let detail = document.getElementById("detail");
+// detail.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   alert("Please select your");
+// });
+// let bouton = document.getElementsByClassName("bouton");
+// bouton.addEventListener("click", () => {
+//   launchThanksModale();
+// });
+
+
 function validate() {
-  launchThanksModal();
   // Validation Prenom
   if (firstName.value === "") {
     firstNameError.innerHTML = "Votre prénom doit contenir 2 caractères ou plus.";
@@ -125,9 +153,20 @@ if(valid) {
 if ((!valid||!testDate||!testEmail||!checkbox.checked)||(lastName.value === "")||(firstName.value === "")||(nbrTournaments.value === "")) {
   return false;
 }
+return true;
 
-  return true;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
