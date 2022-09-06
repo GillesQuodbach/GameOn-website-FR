a7af9a9310a865bb877d201fb522770c37be6dd7
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-09-06 08:44:17 +0200

    Corrections

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,15 +18,15 @@ const lastName = document.querySelector("#lastName"); // Nom
 const email = document.querySelector("#email"); //Email
 const birthdate = document.querySelector("#birthdate"); //Date de naissance
 const nbrTournaments = document.querySelector("#quantity"); //Nbr tournois
-const radioButton = document.forms[0].location;//Radio button
+const radioButton = document.forms[0].location; //Radio button
 const checkbox = document.querySelector("#checkbox1");
 
 //Message d'erreur inputs
 const emailError = document.querySelector(".mail-error"); //Erreur email
-const firstNameError = document.querySelector('.firstname-error'); //Erreur prénom
-const lastNameError = document.querySelector('.lastname-error'); //Erreur nom
-const tournamentError = document.querySelector('.tournament-error'); //Erreur nbr tournois
-const radioError = document.querySelector(".radio-button-error");//Erreur checkbox
+const firstNameError = document.querySelector(".firstname-error"); //Erreur prénom
+const lastNameError = document.querySelector(".lastname-error"); //Erreur nom
+const tournamentError = document.querySelector(".tournament-error"); //Erreur nbr tournois
+const radioError = document.querySelector(".radio-button-error"); //Erreur checkbox
 const birthdateError = document.querySelector(".birthdate-error");
 const checkboxError = document.querySelector(".checkbox-error");
 
@@ -34,7 +34,7 @@ const checkboxError = document.querySelector(".checkbox-error");
 // MAIL REGEXP
 const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 //DATE REGEXP
-const dateRegExp =/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
+const dateRegExp = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 
 //FORMULAIRE D'INSCRIPTION
 // launch modal event
@@ -57,20 +57,23 @@ function launchThxModal() {
   thxModal.style.display = "block";
 }
 
-// Submit formulaire 
-myForm.addEventListener("submit", function(e){
+// Submit formulaire
+myForm.addEventListener("submit", function (e) {
   e.preventDefault();
   if (validate(myForm) === true) {
     launchThxModal();
-    setTimeout(() => {myForm.submit()}, 2000);}
+    setTimeout(() => {
+      myForm.submit();
+    }, 2000);
   }
-);
+});
 
 //VALIDATION FORMULAIRE
 function validate() {
   // Validation Prenom
   if (firstName.value === "") {
-    firstNameError.innerHTML = "Votre prénom doit contenir 2 caractères ou plus.";
+    firstNameError.innerHTML =
+      "Votre prénom doit contenir 2 caractères ou plus.";
     // return false;
   }
   // Validation Nom
@@ -80,7 +83,7 @@ function validate() {
   }
   //Validation Mail
   let testEmail = emailRegExp.test(email.value);
-  if ((testEmail === false) || (email.value === "")) {
+  if (testEmail === false || email.value === "") {
     emailError.innerHTML = "Veuillez saisir une adresse email valide.";
     // return false;
   }
@@ -98,49 +101,34 @@ function validate() {
   //Validation Radio Button
   //Boucle vérifier
   let valid = false;
-for(let i = 0; i < radioButton.length; i++) {
-  if (radioButton[i].checked){
-    valid = true;
-    break;
+  for (let i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].checked) {
+      valid = true;
+      break;
+    }
   }
-}
-if(valid) {
-  radioError.innerHTML = ""
-} else {
-  radioError.innerHTML = "Veuillez choisir une option";
-  // return false;
-}
-//Validation Checkbox
-  if (checkbox.checked === false) {
-    checkboxError.innerHTML = "Veuillez lire et valider les conditions."
+  if (valid) {
+    radioError.innerHTML = "";
+  } else {
+    radioError.innerHTML = "Veuillez choisir une option";
     // return false;
   }
-if ((!valid||!testDate||!testEmail||!checkbox.checked)||(lastName.value === "")||(firstName.value === "")||(nbrTournaments.value === "")) {
-  return false;
+  //Validation Checkbox
+  if (checkbox.checked === false) {
+    checkboxError.innerHTML = "Veuillez lire et valider les conditions.";
+    // return false;
+  }
+  if (
+    !valid ||
+    !testDate ||
+    !testEmail ||
+    !checkbox.checked ||
+    lastName.value === "" ||
+    firstName.value === "" ||
+    nbrTournaments.value === ""
+  ) {
+    return false;
+  }
+
+  return true;
 }
-
-return true;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
